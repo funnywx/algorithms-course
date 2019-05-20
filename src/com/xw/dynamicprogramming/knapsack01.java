a9b036874b77d78f77da8d41f4aff6322923d856
geekmanx <1776031626@qq.com>
2019-05-20 21:31:01 +0800

    01 knapsack problem

--- src/com/xw/dynamicprogramming/knapsack01.java
+++ src/com/xw/dynamicprogramming/knapsack01.java
@@ -7,7 +7,40 @@ package com.xw.dynamicprogramming;
  */
 public class knapsack01 {
 
+    private static int[] W = {0,2,3,4,5};
+    private static int[] V = {0,3,4,5,6};
 
+    /**
+     * 递归式i为物品 j为当前背包剩余容量
+     * V(i,j) = V(i-1,j) , j < Wi
+     *          Max( V(i-1,j) , V(i-1,j-Wi) + Vi) ) j>=Wi
+     */
+    public static int[][] sovle(int[] W,int[] V,int bagW){
+        int n = W.length;
+        int[][] a = new int[n][bagW+1];
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j <= bagW ; j++){
+                if(W[i] > j) a[i][j] = a[i-1][j];
+                if(W[i] <= j) a[i][j] = a[i-1][j-W[i]] + V[i];
+                printa(a);
+            }
+        }
+        return a;
+    }
+
+    private static void printa(int[][] a){
+        for(int i = 0; i < a.length ; i++){
+            for(int j = 0;j < a[0].length ;j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------");
+    }
+
+    public static void main(String[] args) {
+        int[][] a = sovle(W,V,8);
+    }
 
 
 
